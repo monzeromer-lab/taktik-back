512cd5ba3842e7cc770c4a6c04d419fce53cc4af
Monzer Abdullaziz <monzer.a.omer@gmail.com>
2023-09-08 18:34:17 +0200

    fixed diesel error

--- src/models/user.rs
+++ src/models/user.rs
@@ -1,15 +1,15 @@
-// use std::time::SystemTime;
+use std::time::SystemTime;
 
-// use diesel::prelude::Queryable;
+use diesel::prelude::Queryable;
 
-// #[derive(Queryable, PartialEq, Debug)]
-// pub struct User {
-//     id: i32,
-//     name: String,
-//     email: String,
-//     password: String,
-//     image: String,
-//     created_at: SystemTime,
-//     updated_at: SystemTime,
-//     deleted_at: SystemTime,
-// }
+#[derive(Queryable, PartialEq, Debug)]
+pub struct User {
+    id: i32,
+    name: String,
+    email: String,
+    password: String,
+    image: String,
+    created_at: SystemTime,
+    updated_at: SystemTime,
+    deleted_at: SystemTime,
+}
