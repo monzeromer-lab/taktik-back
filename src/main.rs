ccf6e6f34cbcf0850fdbc8879780f187206b5e10
Monzer Abdullaziz <monzer.a.omer@gmail.com>
2023-09-08 10:07:01 +0200

    removed dummy hello route, LOL

--- src/main.rs
+++ src/main.rs
@@ -8,17 +8,11 @@ use actix_web::{get, App, HttpResponse, HttpServer, Responder};
 
 use crate::users::users_module;
 
-#[get("/")]
-async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello world!")
-}
-
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     println!("Taktik server is running at: http://127.0.0.1:8080");
     HttpServer::new(|| {
         App::new()
-            .service(hello)
             .service(users_module())
     })
     .bind(("0.0.0.0", 8080))?
