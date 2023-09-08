ccf95fb27c1e7f424a28eefd9cb6f1c505751f9e
Monzer Abdullaziz <monzer.a.omer@gmail.com>
2023-09-08 06:24:05 +0200

    removed unused code

--- src/routers/mod.rs
+++ src/routers/mod.rs
@@ -1,4 +1,4 @@
-use actix_web::{dev::HttpServiceFactory, services, web, App};
+use actix_web::{dev::HttpServiceFactory, services, web};
 
 pub fn users_module() -> impl HttpServiceFactory {
     let services = services![
