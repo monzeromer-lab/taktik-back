efcb78b3499309022a8d55ac6ef1cd23938dfc66
Monzer Omer <monzer.a.omer@gmail.com>
2023-10-07 00:00:49 +0200

    did i mention that i hate my life?

--- src/shared/crud.rs
+++ src/shared/crud.rs
@@ -1,7 +1,6 @@
 pub struct CRUD;
 
-use sea_orm::*;
-use sea_orm::entity::*;
+use sea_orm::{*, sea_query::IntoCondition};
 
 
 impl CRUD {
@@ -9,8 +8,25 @@ impl CRUD {
 
     }
 
-    pub async fn read<DBConnection: ConnectionTrait, Model: EntityTrait, Data>(data: Option<Box<Data>>, connection: Option<DBConnection>){
-        Model::find().all(Some(&connection.unwrap()).unwrap()).await;
+    // pub async fn read<DBConnection: ConnectionTrait, Model: EntityTrait, Data>(data: Option<Box<Data>>, connection: Option<DBConnection>) -> Vec<Model>{
+    //     let result: Result<Vec<Model>, DbErr> = Model::find().all(Some(&connection.unwrap()).unwrap()).await.map_err(|_err| ());
+    //     result
+    // }
+    pub async fn read<Connection, Model, Data>(
+        data: Option<Data>,
+        connection: Option<&Connection>,
+    ) -> Result<Vec<<Model as EntityTrait>::Model>, sea_orm::error::DbErr>
+    where
+        Connection: sea_orm::ConnectionTrait,
+        Model: EntityTrait,
+        Data: IntoCondition,
+    {
+        let query = match data {
+            Some(filter) => Model::find().filter(filter),
+            None => Model::find(),
+        };
+    
+        query.all(connection.unwrap()).await
     }
 
     pub async fn update<Connection: ConnectionTrait, Model: EntityTrait, UpdateWhere, Data>(update_at: UpdateWhere, data: Data){
